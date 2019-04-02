630e58a1c856baeb66739fa85ff8c84f322df9c3
Darian <dariann6@gmail.com>
2019-04-01 22:09:07 -0700

    Reordered main.js components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+const request = new XMLHttpRequest();
+const optionList = document.querySelector("#option-list1");
+
+const searchBar = document.querySelector("#search-bar");
+searchBar.addEventListener("input", function() {
+    request.open("GET", `https://www.ifixit.com/api/2.0/suggest/${searchBar.value}?doctypes=device`, true);
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            //clear search results
+            while (optionList.firstChild) {
+                optionList.removeChild(optionList.firstChild);
+            }
+
+            if (searchBar.value === "") {
+                return;
+            }
+            const data = JSON.parse(this.response);
+            data["results"].forEach(result => {
+                const option = document.createElement("li");
+                option.setAttribute("class", "option");
+                option.textContent = result.title;
+
+                optionList.appendChild(option);
+            });
+        } else {
+            console.log("error loading categories"); //TODO Issue #6
+        }
+    }
+    request.send();
+});
+
+/* category chooser
 function loadCategories(devicesData, devices) {
     for (let entry in devicesData) {
         const device = document.createElement("li");
@@ -8,48 +40,13 @@ function loadCategories(devicesData, devices) {
     }
 }
 
-function main() {
-    const request = new XMLHttpRequest();
-    const optionList = document.querySelector("#option-list1");
-    
-    //load categories
-    /*request.open("GET", "https://www.ifixit.com/api/2.0/categories", true);
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-            loadCategories(JSON.parse(this.response), devices);
-        } else {
-            console.log("error loading data"); //TODO Issue #6
-        }
-    };
-    request.send();*/
 
-    const searchBar = document.querySelector("#search-bar");
-    searchBar.addEventListener("input", function() {
-        request.open("GET", `https://www.ifixit.com/api/2.0/suggest/${searchBar.value}?doctypes=device`, true);
-        request.onload = function () {
-            if (request.status >= 200 && request.status < 400) {
-                //clear search results
-                while (optionList.firstChild) {
-                    optionList.removeChild(optionList.firstChild);
-                }
-
-                if (searchBar.value === "") {
-                    return;
-                }
-                const data = JSON.parse(this.response);
-                data["results"].forEach(result => {
-                    const option = document.createElement("li");
-                    option.setAttribute("class", "option");
-                    option.textContent = result.title;
-
-                    optionList.appendChild(option);
-                });
-            } else {
-                console.log("error loading categories"); //TODO Issue #6
-            }
-        }
-        request.send();
-    });
-}
-
-main();
+request.open("GET", "https://www.ifixit.com/api/2.0/categories", true);
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        loadCategories(JSON.parse(this.response), devices);
+    } else {
+        console.log("error loading data"); //TODO Issue #6
+    }
+};
+request.send();*/
