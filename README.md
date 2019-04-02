f1c55579deea944297fb198e2b59cfd21a76e6b9
Darian <dariann6@gmail.com>
2019-04-02 11:50:23 -0700

    Refactored how results are added to the DOM

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ifixit-grab-bag
 A tool that uses the iFixit API to allow users to assemble and add a collection of devices they own to their account.
+
+# Notes about iFixit API 2.0
+- */categories/all* contains names of categories & devices, with no way to distinguish between the two. Also has duplicates.
+- */Suggest* also does not distinguish between categories & devices.
+- */wikis/CATEGORY?display=hierarchy* is functionally similar to */categories*
+- */wikis/{namespace}* has limited functionality (e.g response limit).
+
--- js/main.js
+++ js/main.js
@@ -1,52 +1,58 @@
 const request = new XMLHttpRequest();
-const optionList = document.querySelector("#option-list1");
+const resultContainer = document.querySelector("#result-container");
 
 const searchBar = document.querySelector("#search-bar");
 searchBar.addEventListener("input", function() {
-    request.open("GET", `https://www.ifixit.com/api/2.0/suggest/${searchBar.value}?doctypes=device`, true);
+    if (searchBar.value !== "") {
+        loadSearchResults(searchBar.value);
+    }
+});
+
+function loadSearchResults(searchQuery) {
+    request.open("GET", `https://www.ifixit.com/api/2.0/suggest/${searchQuery}?doctypes=device`, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             //clear search results
-            while (optionList.firstChild) {
-                optionList.removeChild(optionList.firstChild);
+            while (resultContainer.firstChild) {
+                resultContainer.removeChild(resultContainer.firstChild);
             }
 
-            if (searchBar.value === "") {
-                return;
-            }
+            const resultList = document.createElement("ul");
+            resultList.setAttribute("class", "option-list");
+            resultContainer.appendChild(resultList);
+
             const data = JSON.parse(this.response);
             data["results"].forEach(result => {
                 const option = document.createElement("li");
                 option.setAttribute("class", "option");
                 option.textContent = result.title;
 
-                optionList.appendChild(option);
+                resultList.appendChild(option);
             });
         } else {
             console.log("error loading categories"); //TODO Issue #6
         }
     }
     request.send();
-});
-
-/* category chooser
-function loadCategories(devicesData, devices) {
-    for (let entry in devicesData) {
-        const device = document.createElement("li");
-        device.setAttribute("class", "option");
-        device.textContent = entry;
-    
-        devices.appendChild(device);
-    }
 }
 
-
-request.open("GET", "https://www.ifixit.com/api/2.0/categories", true);
-request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-        loadCategories(JSON.parse(this.response), devices);
-    } else {
-        console.log("error loading data"); //TODO Issue #6
-    }
-};
-request.send();*/
+/* category chooser
+function loadOptions(list) {
+    request.open("GET", "https://www.ifixit.com/api/2.0/categories", true);
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            const data = JSON.parse(this.response);
+            for (let entry in data) {
+                const option = document.createElement("li");
+                option.setAttribute("class", "option");
+                option.textContent = entry;
+            
+                list.appendChild(option);
+            }
+        } else {
+            console.log("error loading data"); //TODO Issue #6
+        }
+    };
+    request.send();
+    
+}*/
